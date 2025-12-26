0d46fa8ef99273cf9088addc9e0845016ee9ecd3
Alper Tunçkol <47117600-alperwork91@users.noreply.replit.com>
2025-12-26 13:40:09 +0000

    Insert Excel data into database table

--- src/exceltosqlbot.py
+++ src/exceltosqlbot.py
@@ -23,24 +23,35 @@ print(df.iloc[:, :8])
 engine = create_engine('sqlite:///trades.db', echo=True)
 Base = declarative_base()
 
+
 class Stock(Base):
-__tablename__ = 'trades'
-id = Column(Integer, primary_key=True)
-trade_id = Column(Integer)
-symbol = Column(String)
-trade_date = Column(Date)
-side = Column(String)
-quantity = Column(Integer)
-price = Column(Float)
-trader = Column(String)
-strategy = Column(String)
+  __tablename__ = 'trades'
+  id = Column(Integer, primary_key=True)
+  trade_id = Column(Integer)
+  symbol = Column(String)
+  trade_date = Column(Date)
+  side = Column(String)
+  quantity = Column(Integer)
+  price = Column(Float)
+  trader = Column(String)
+  strategy = Column(String)
+
 
 Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
-objects = [MyData(col1=row[0], col2=row[1]) for row in df.values]
+objects = [
+    Stock(trade_id=row[0],
+          symbol=row[1],
+          trade_date=row[2],
+          side=row[3],
+          quantity=row[4],
+          price=row[5],
+          trader=row[6],
+          strategy=row[7]) for row in df.values
+]
 
 session.add_all(objects)
 
